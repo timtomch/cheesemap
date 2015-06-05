3a19bfccbd9425a697d4084edc8ca39e84a46b28
Thomas Guignard <tom@timtom.ch>
2015-06-05 14:00:11 -0400

    Initial commit - without API key

---
+++ cheesemap.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+from googleplaces import GooglePlaces, types, lang
+import csv
+
+YOUR_API_KEY = ''
+
+google_places = GooglePlaces(YOUR_API_KEY)
+
+def geocode(name, province):
+    query = name + " " + province
+    query_result = google_places.text_search(query)
+    
+    result_places = query_result.places
+    #print result_places
+    #print len(result_places)
+    
+    if len(result_places)>0:
+        # Just get first result (we're feeling lucky)
+        result = query_result.places[0]
+        return result.geo_location
+    else:
+        return False
+    #print result.geo_location
+    #print result.place_id
+
+
+
+with open('canadianCheeseDirectory.csv','rU') as csvinput:
+    with open('output.csv', 'w') as csvoutput:
+        writer = csv.writer(csvoutput, lineterminator='\n')
+        reader = csv.reader(csvinput)
+
+        all = []
+        row = next(reader)
+        row.append('Coordinates')
+        all.append(row)
+        previousname = ""
+
+        for row in reader:
+            
+            #Check if English name (4th row) is set, if not use French name (5th row)
+            dairyname = row[3]
+            if not dairyname:
+                dairyname = row[4]
+            
+            province = row[5]
+                
+            # Run the geocoder only once per dairy, to minimize API calls    
+            if previousname != dairyname:
+                previousname = dairyname
+                #coordinates = geocode(dairyname,province)
+                coordinates = "test"
+                row.append(coordinates)
+            else:    
+                # If dairy name is the same as the previous one, reuse the coordinates
+                row.append(coordinates)
+            
+            print dairyname
+            print coordinates
+            
+            all.append(row)
+
+        writer.writerows(all)
+
+
+
+
+# def csv_writer(data, path):
+#     with open(path, "wb") as csv_file:
+#         writer = csv.writer(csv_file, delimiter=',')
+#         for line in data:
+#             writer.writerow(line)
+#
+#
+# raw = open("canadianCheeseDirectory.csv", "rU")
+# cooked = csv.reader(raw)
+#
+# data = []
+#
+# for row in cooked:
+#     geoloc = geocode(row[0],row[1])
+#     #geoloc = 0
+#     data.append([row[0],row[1],geoloc])
+#
+# print data
+#
+# path = "out.csv"
+# csv_writer(data,path)
+
