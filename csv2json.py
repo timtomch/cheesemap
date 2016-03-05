1a605258b4191b04777b75a0150163a05a9ee2dd
Thomas Guignard <tom@timtom.ch>
2016-03-04 23:29:45 -0500

    geoJSON export working
    To fix:
    - Check why lat&lon are swapped
    - Display properties more nicely

--- csv2json.py
+++ csv2json.py
@@ -3,62 +3,47 @@
 
 import csv
 import sys
+import geojson
 
 
 def csv2json(infile, outfile):
 
     # Read in raw data from csv
     rawData = csv.reader(open(infile, 'rb'))
-
-    # the template. where data from the csv will be formatted to geojson
-    template = \
-        ''' \
-        { "type" : "Feature",
-            "geometry" : {
-                    "type" : "Point",
-                    "coordinates" : ["%s","%s"]},
-            "properties" : { "dairy" : "%s", "cheese" : "%s"}
-            },
-        '''
-
-    # the head of the geojson file
-    output = \
-        ''' \
-    { "type" : "Feature Collection",
-        {"features" : [
-        '''
+    
+    # Create empty list to contain all entries
+    featureslist=[]
 
     # loop through the csv by row skipping the first
     iter = 0
     for row in rawData:
         iter += 1
         if iter >= 2:
-            lat = row[30]
-            lon = row[31]
-            
-            dairyname = row[3]
-            if not dairyname:
-                dairyname = row[4]
-            
-            cheesename = row[1]
-            if not cheesename:
-                cheesename = row[2]
             
             # only write out geolocated records
-            if len(lat)>0:
-                output += template % (lat,lon,dairyname,cheesename)
-        
-    # the tail of the geojson file
-    output += \
-        ''' \
-        ]
-    }
-        '''
+            if (len(row[30])>0 and len(row[31])>0):
+            
+                lat = float(row[31])
+                lon = float(row[30])
+            
+                dairyname = row[3]
+                if not dairyname:
+                    dairyname = row[4]
+            
+                cheesename = row[1]
+                if not cheesename:
+                    cheesename = row[2]
+            
+                newfeature = geojson.Feature(geometry=geojson.Point((lat, lon)),properties={"dairy":dairyname, "cheese":cheesename})
+                featureslist.append(newfeature)
+                
+    # Group all features into a collection
+    collection = geojson.FeatureCollection(featureslist);
     
-    # opens an geoJSON file to write the output to
-    outFileHandle = open(outfile, "w")
-    outFileHandle.write(output)
-    outFileHandle.close()
+    # Export collection as geoJSON file
+    with open(outfile, 'w') as jsonout:
+         geojson.dump(collection, jsonout)
+
 
 if len(sys.argv) < 3:
     print "Please provide both input and output files."
